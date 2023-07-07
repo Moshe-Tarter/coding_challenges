3803dc087f6dbb00dc2604f09ee4aba67f575987
Moshe <moshe.tarter@sightdx.com>
2023-07-07 16:42:34 +0300

    solution for the longest substring no repeates, need to add unit tests

---
+++ medium/longest_substring_no_rep.py
@@ -0,0 +1,60 @@
+import typing
+
+
+'''
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
+
+def longest_substr(s: str) -> int:
+    lookup = set()
+    start = temp_start = 0
+    count = temp_count = 0
+
+    if s == '':
+        return 0
+    else:
+        for i, char in enumerate(s):
+            if char not in lookup:
+                lookup.add(char)
+                temp_count += 1
+                if temp_count > count:
+                    count = temp_count
+                    start = temp_start
+            else:
+                lookup.clear()
+                temp_start = s[temp_start:i].index(char) + 1 + temp_start
+                print(temp_start)
+                temp_count = 0
+                for ch in s[temp_start: i+1]:
+                    lookup.add(ch)
+                    temp_count += 1
+
+    return len(s[start: start+count])
+
+    
+if __name__ == '__main__':
+    print(longest_substr('thixq is some sting^%13'))
